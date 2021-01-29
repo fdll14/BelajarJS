a190e0422a43f2d2c1e93e681325bf56ea6acbbf
Muhamad Shuro Fadhillah <shuro.fadhillah@gmail.com>
2021-01-30 01:11:05 +0700

    tugas latihan minggu pertama

---
+++ tugasPertemuan1.js
@@ -0,0 +1,21 @@
+function checkGrade(nilai){
+
+  if ( nilai > 79 && nilai <= 100) {
+    console.log("A");
+  } else if(nilai > 65 && nilai <= 79) {
+    console.log("B");
+  } else if(nilai > 50 && nilai <=65 ){
+    console.log("C");
+  } else if(nilai <= 50 ){
+    console.log("D");
+  }else{
+    console.log("Anda Terlalu Cerdas!");
+  }
+
+}
+
+checkGrade((80))
+checkGrade((79))
+checkGrade((65))
+checkGrade((50))
+checkGrade((500))
